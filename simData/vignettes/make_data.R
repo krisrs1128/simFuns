2d7f6c567e9dbf6b77967c8239e08a99b37d9295
Kris Sankaran <sankaran.kris@gmail.com>
2016-03-09 11:36:21 -0800

    use smaller samples size, so can look at text of points

--- simData/vignettes/make_data.R
+++ simData/vignettes/make_data.R
@@ -9,10 +9,10 @@ path <- "~/Documents/programming/simFuns/"
 setwd(path)
 
 ## pls-data ----
-pls_two_table <- ppls_shared(list(n = 5000, p = 40))
+pls_two_table <- ppls_shared(list(n = 25, p = 10))
 pairs(pls_two_table$X[[1]][, c(1:5)]) # first five columns of first table
 pairs(cbind(pls_two_table$X[[1]][, c(1:4)], pls_two_table$X[[2]][, c(1:4)])) # first five columns, across first two tables
 use_data(pls_two_table, pkg = "simData", overwrite = T)
 
-pls_four_table <- ppls_shared(list(n = 5000, p = 40, k_unique = rep(2, 4)))
+pls_four_table <- ppls_shared(list(n = 25, p = 10, k_unique = rep(2, 4)))
 use_data(pls_four_table, pkg = "simData", overwrite = T)
