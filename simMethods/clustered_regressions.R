3929d7c8cb74baeb8611d895e7de14d527625fe2
Kris Sankaran <sankaran.kris@gmail.com>
2016-03-18 13:31:46 -0700

    write description of clustered regressions, and generate Y

--- simMethods/clustered_regressions.R
+++ simMethods/clustered_regressions.R
@@ -2,6 +2,9 @@
 ################################################################################
 # Clustered regression models, a special case of
 # "Flexible latent variable models for multi-task learning"
+#
+# b[r] for r = 1:p[1] is a noisy realization of a much smaller number of
+# b[1], ..., b[k]
 ################################################################################
 
 ## ---- libraries ----
@@ -15,11 +18,13 @@ opts$p <- c(60, 20) # 60 response dimensions, 20 features
 opts$k <- 4
 opts$sigma_s0 <- 2
 opts$sigma_psi <- .1
+opts$sigma_x0 <- 2
+opts$sigma_y <- 4
 
 ## ---- fixed-params ----
 S <- matnorm(opts$p[2], opts$k, opts$sigma_s0)
 Psi <- matnorm(opts$p[2], opts$p[2], opts$sigma_psi)
-Psi <- Psi %*% t(Psi) # random wishart
+Psi <- t(Psi) %*% Psi # random wishart
 
 ## ---- random-params ----
 W <- sample(1:opts$k, opts$p[1], replace = T) # each equally likely
@@ -31,3 +36,8 @@ B <- S %*% W + sqrtm(Psi) %*% E
 
 W[, 1:8]
 pairs(B[, 1:8], B[, 1:8])
+
+## ---- generate-data ----
+X <- matnorm(opts$n, opts$p[2], opts$sigma_x0)
+Y <- X %*% B + matnorm(opts$n, opts$p[1], opts$sigma_y)
+plot(S[, which(W[, 1] == 1)], coef(lm(Y[, 1] ~ X - 1)))
