d1ab3847993b1fe47f985410a7d1a72df2f82f10
Kris Sankaran <sankaran.kris@gmail.com>
2016-04-04 15:38:26 -0700

    prepend file name to chunks, so no issues mixing chunks between files in writeup

--- simMethods/sparse_sim.R
+++ simMethods/sparse_sim.R
@@ -3,7 +3,7 @@
 # Simulate data according to Daniela Witten's PMD paper
 ################################################################################
 
-## ---- packages ----
+## ---- sparse-sim-packages ----
 library("reshape2")
 library("ggplot2")
 library("dplyr")
@@ -13,7 +13,7 @@ library("vegan")
 library("FactoMineR")
 theme_set(theme_bw())
 
-## ---- opts ----
+## ---- sparse-sim-opts ----
 opts <- list()
 opts$n <- 504 # want divisible by 8
 opts$p <- 20
@@ -22,7 +22,7 @@ opts$l <- 2
 opts$sigma0 <- 5
 opts$sigma <- 1
 
-## ---- source-scores ----
+## ---- sparse-sim-source-scores ----
 S <- qr.Q(qr(matnorm(opts$p, opts$k, opts$sigma0))) # same source between the two matrices
 W <- replicate(opts$l, matrix(0, opts$n, opts$k), simplify = F)
 W[[1]][, 1] <- c(rep(10, opts$n / 8), rep(-10, opts$n / 8),
@@ -33,12 +33,12 @@ W[[1]][, 2] <- c(rep(10, opts$n / 4), rep(-10, opts$n / 4),
 W[[2]][, 1] <- c(rep(0, opts$n / 4), rep(10, opts$n / 2), rep(-10, opts$n / 4))
 W[[2]][, 2] <- c(rep(-10, opts$n / 4), rep(10, opts$n / 4), rep(-10, opts$n / 4), rep(10, opts$n / 4))
 
-## ---- generate-data ----
+## ---- sparse-sim-generate-data ----
 X <- common_source_model(W, S, opts)
 X <- lapply(X, scale)
 pairs(X[[1]][, 1:4])
 
-## ---- separate-pcas ----
+## ---- sparse-sim-separate-pcas ----
 pca_sep <- lapply(X, princomp)
 pca_sep_scores <- lapply(pca_sep, function(x) x$scores)
 Mu <- W
@@ -50,7 +50,7 @@ ggplot(D %>% filter(comp < 4)) +
   geom_point(aes(x = ix, y = value, col = as.factor(comp), shape = type), alpha = 0.6, size = 1) +
   facet_grid(type ~ table ~ comp, scale = "free_y")
 
-## ---- concatenated-pca ----
+## ---- sparse-sim-concatenated-pca ----
 pca_concat <- princomp(do.call(cbind, X))
 
 D <- melt(list(Mu = Mu, pca_concat = pca_concat$scores))
@@ -60,7 +60,7 @@ ggplot(D %>% filter(comp < 4)) +
   geom_point(aes(x = ix, y = value, col = as.factor(comp), shape = type), alpha = 0.6, size = 1) +
   facet_wrap(type ~ comp, scale = "free_y")
 
-## ---- pmd-unordered ----
+## ---- sparse-sim-pmd-unordered ----
 pmd_res <- MultiCCA(lapply(X, function(x) t(x)), penalty = 10, ncomponents = 3)
 D2 <- melt(list(Mu = Mu, pmd_sep = pmd_res$ws))
 colnames(D2) <- c("ix", "comp", "value", "table", "type")
@@ -68,7 +68,7 @@ ggplot(D2 %>% filter(comp < 4)) +
   geom_point(aes(x = ix, y = value, col = as.factor(comp), shape = type), alpha = 0.6, size = 1) +
   facet_grid(type ~ table ~ comp, scale = "free_y")
 
-## ---- pmd-ordered ----
+## ---- sparse-sim-pmd-ordered ----
 pmd_res <- MultiCCA(lapply(X, function(x) t(x)), penalty = 1, type = "ordered",
                     ncomponents = 3)
 D3 <- melt(list(Mu = Mu, pmd_sep = pmd_res$ws))
@@ -78,7 +78,7 @@ ggplot(D3 %>% filter(comp < 4)) +
              size = 1) +
   facet_grid(type ~ table ~ comp, scale = "free_y")
 
-## ---- mfa ----
+## ---- sparse-sim-mfa ----
 mfa_res <- MFA(do.call(cbind, X), group = sapply(X, ncol))
 D <- melt(list(Mu = Mu,
                mfa_group_one = mfa_res$separate.analyses$group.1$ind$coord[, 1:3],
@@ -90,7 +90,7 @@ ggplot(D %>% filter(comp < 4)) +
              size = 1) +
   facet_grid(type ~ table ~ comp, scale = "free_y")
 
-## ---- cca ----
+## ---- sparse-sim-cca ----
 cca_res <- vegan::CCorA(X[[1]], X[[2]])
 D <- melt(list(Mu = Mu,
                cca_x1 = cca_res$Cy[, 1:3],
