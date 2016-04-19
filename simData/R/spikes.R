250c1b83ea28107c3baecf115832766a715f6f38
Kris Sankaran <sankaran.kris@gmail.com>
2016-04-19 14:03:29 -0700

    sum of spikes

--- simData/R/spikes.R
+++ simData/R/spikes.R
@@ -3,7 +3,7 @@
 # Functions for simulating spiky data
 ################################################################################
 
-#' @title Functional for creating spikes
+#' @title Functional for creating symmetric spikes
 #' @description Returns functions that can be evaluated on x and returns f(x)
 #' where f is zero outside of an interval, and is a symmetric spike within that
 #' interval.
@@ -27,5 +27,30 @@ symm_spike <- function(x0, h, w) {
     fx[left_ix] <- (h / w) * (x[left_ix] - x0) + h
     fx[right_ix] <- - (h / w) * (x[right_ix] - x0) + h
     fx
+  }
+}
+
+#' @title Functional for creating sum of symmetric spikes
+#' @description Returns functions that can be evaluated on x and returns f(x)
+#' where f is is a sum of symmetric spike functions.
+#' @param x0 The centerpoints for the peak.
+#' @param h The maximum peak heights.
+#' @param w The half-widths of the intervals.
+#' @export
+#' @examples
+#' x0 <- c(3, 15, 7)
+#' h <- c(5, 3, 4)
+#' w <- c(1, 4, .5)
+#' f <- symm_spike_sum(x0, h, w)
+#' x <- seq(2, 20, .01)
+#' plot(x, f(x))
+symm_spike_sum <- function(x0, h, w) {
+  stopifnot(length(x0) == length(h) && length(x0) == length(w))
+  function(x) {
+    fx <- matrix(0, length(x), length(x0))
+    for (j in seq_along(x0)) {
+      fx[, j] <- symm_spike(x0[j], h[j], w[j])(x)
+    }
+    rowSums(fx)
   }
 }
