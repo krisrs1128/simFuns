c56d5fabef9f1c82285a7015f8c952fcd81b8a50
Kris Sankaran <sankaran.kris@gmail.com>
2016-05-08 15:14:23 -0700

    clustered regression analogous to gflasso

--- simMethods/clustered_regressions.R
+++ simMethods/clustered_regressions.R
@@ -8,25 +8,40 @@
 ################################################################################
 
 ## ---- libraries ----
-library("simData")
 library("expm")
 library("plyr")
 library("reshape2")
 library("bayesMult")
 library("ggplot2")
 library("dplyr")
+
 theme_set(theme_bw())
+min_theme <- theme(panel.border = element_blank(),
+                   panel.grid = element_blank(),
+                   axis.ticks = element_blank(),
+                   legend.title = element_text(size = 10),
+                   axis.text = element_text(size = 8),
+                   axis.title = element_text(size = 10),
+                   strip.background = element_blank(),
+                   strip.text = element_text(size = 10),
+                   legend.key = element_blank())
 
 ## ---- opts ----
 opts <- list()
-opts$n <- 200
-opts$p <- c(100, 2) # 60 response dimensions, 20 features
+opts$n <- 15
+opts$p <- c(100, 10)
 opts$k <- 3
 opts$sigma_b <- 1
-opts$sigma_y <- 4
+opts$sigma_s <- 2
+opts$sigma_y <- 2
+set.seed(5052016)
 
 ## ---- fixed-params ----
-S <- matrix(c(0, 2, 3, 1, 2, 4), nrow = 2)
+S <- matrix(rnorm(opts$p[2] * opts$k, 0, sd = opts$sigma_s),
+            opts$p[2], opts$k)
+#S[1:(opts$p[2] / 2), ] <- S[1:(opts$p[2] / 2)] + 1
+#S[(opts$p[2] / 2 + 1):opts$p[2], ] <- S[(opts$p[2] / 2 + 1):opts$p[2], ] - 1
+#S <- S[sample(1:opts$p[2]), ]
 Psi <- diag(opts$p[2])
 
 ## ---- random-params ----
@@ -34,8 +49,21 @@ W <- sample(1:opts$k, opts$p[1], replace = T) # each equally likely
 dfw <- data.frame(w = as.factor(W))
 W <- t(model.matrix(~ w - 1, dfw, intercept = F))
 
-E <- matnorm(opts$p[2], opts$p[1], opts$sigma_b)
+E <- matrix(rnorm(prod(opts$p), sd = opts$sigma_b), opts$p[2], opts$p[1])
 B <- S %*% W + sqrtm(Psi) %*% E
+true_clust <- setNames(apply(W, 2, which.max), 1:opts$p[1])
+
+## ---- plot-B ----
+m_b <- melt(B)
+m_b$true_clust <- true_clust[m_b$Var2]
+
+ggplot(m_b) +
+  geom_tile(aes(y = Var1, x = reorder(Var2, true_clust), fill = value)) +
+  scale_fill_gradient2(midpoint = 0, high = "#90ee90", low = "#000080") +
+  min_theme +
+  xlab("Tasks [reordered]") +
+  ylab("Features") +
+  theme(axis.text.x = element_text(angle = -90))
 
 ## ---- generate-data ----
 x_list <- list()
@@ -43,7 +71,8 @@ y_list <- list()
 
 unique_ts <- list()
 for(p in seq_len(opts$p[2])) {
-  unique_ts[[p]] <- arima.sim(list(order = c(1,1,0), ar = 0.7), n = opts$n)[-1]
+  #unique_ts[[p]] <- arima.sim(list(order = c(1,1,0), ar = 0.7), n = opts$n)[-1]
+  unique_ts[[p]] <- rnorm(opts$n)
 }
 
 for(r in seq_len(opts$p[1])) {
@@ -54,65 +83,56 @@ for(r in seq_len(opts$p[1])) {
   y_list[[r]] <- x_list[[r]] %*% B[, r] + rnorm(opts$n, 0, opts$sigma_y)
 }
 
-## ---- plot-x ----
-mX <- melt(x_list[[1]])
-ggplot(mX) +
-  geom_line(aes(x = Var1, y = value, col = as.factor(Var2))) +
-  ggtitle("Two underlying OTUs")
+## ---- join-data ----
+m_y <- melt(y_list, value.name = "y")
+m_y$Var2 <- NULL
+colnames(m_y) <- c("time", "y", "task")
+m_x <- melt(x_list, value.name = "x")
+colnames(m_x) <- c("time", "feature", "x", "task")
+colnames(m_b) <- c("feature", "task", "slope", "cluster")
 
-## ---- plot-y ----
-mY <- melt(y_list)
-mB <- melt(B) %>%
-  dcast(Var2 ~ Var1)
-colnames(mB) <- c("L1", "beta1", "beta2")
-mY <- mY %>%
-  left_join(mB)
-ggplot(mY) +
-  geom_line(aes(x = Var1, y = value, group = L1, col = beta1)) +
-  ggtitle("Genes, colored by coef. first OTU")
-ggplot(mY) +
-  geom_line(aes(x = Var1, y = value, group = L1, col = beta2)) +
-  ggtitle("Genes, colored by coef. second OTU")
+merged_data <- m_x %>%
+  left_join(m_y) %>%
+  left_join(m_b)
 
-merged_lists <- lapply(seq_along(x_list), function(i) {
-  data.frame(t = 1:opts$n, y = y_list[[i]], x = x_list[[i]])
-})
-
-m_merged <- melt(merged_lists, id.vars = "t") %>%
-  filter(L1 < 10)
-
-clust <- as.factor(apply(W, 2, which.max))
-m_merged$cluster <- clust[m_merged$L1]
-ggplot(m_merged) +
-  geom_line(aes(x = t, y = value, linetype = variable, col = cluster)) +
-  facet_wrap(~L1) +
-  ggtitle("Clustered Series")
+## ---- vis-reg-lines ----
+ggplot(merged_data %>%
+         filter(task < 10, feature < 5)) +
+  geom_point(aes(x = x, y = y, col = as.factor(cluster)), size = .3) +
+  geom_abline(aes(slope = slope, intercept = 0, col = as.factor(cluster))) +
+  scale_color_manual(values = c("#5FABC8", "#ffdead", "#c16a67")) +
+  facet_grid(feature ~ task) +
+  min_theme
 
 ## ---- run-vb ----
 phi <- rep(1 / opts$k, opts$k)
 sigma <- opts$sigma_y # assume we know sigma for now
 
-S0 <- matrix(c(1.5, 2.5, 2.5, 1.5, .5, 2.0), nrow = 2)
-Psi0 <- diag(runif(opts$p[2], .8, 1.2))
+S0 <- matrix(rnorm(opts$p[2] * opts$k, 0, sd = opts$sigma_s),
+            opts$p[2], opts$k)
+Psi0 <- diag(runif(opts$p[2], .5, 1.5))
 data_list <- list(x_list = x_list, y_list = y_list)
 param_list <- list(S = S0, Psi = Psi0, phi = phi, sigma = sigma)
+vb_res <- vb_multinom(data_list, param_list, 1)
 
-vb_res <- vb_multinom(data_list, param_list, 4)
-
+## ---- plot-pi ----
 pi_guess <- t(vb_res$var_list$pi)
+true_clust <- apply(W, 2, function(x) which(x == 1))
+pi_est <- melt(data.frame(pi = pi_guess, true_clust),
+               measure.vars = paste("pi", 1:opts$k, sep = "."))
+pi_est$true  <- as.factor(pi_est$true)
+
+ggplot(pi_est) +
+  geom_jitter(aes(x = variable, y = value, col = true)) +
+  scale_color_manual(values = c("#5FABC8", "#ffdead", "#c16a67")) +
+  min_theme
+
+## ---- plot-clusters ----
 m <- t(vb_res$var_list$m)
 clust <- as.factor(apply(pi_guess, 1, which.max))
 m_df <- data.frame(m, clust = clust)
-colnames(m_df) <- c("beta1", "beta2", "cluster")
-
-## ---- plot-clusters ----
-ggplot(m_df) +
-  geom_point(aes(x = beta1, y = beta2, col = cluster)) +
-  ggtitle("Recovered Clusters")
-
-## ---- coord-desc ----
-qplot(seq_along(vb_res$elbo), vb_res$elbo) +
-  ggtitle("Objective after each update")
+colnames(m_df) <- c("beta_1", "beta_2", "cluster")
+table(clust, true_clust)
 
 ## ---- naive-model ----
 B_lm <- matrix(0, nrow(B), ncol(B))
@@ -120,29 +140,37 @@ for(r in seq_len(ncol(B))) {
   B_lm[, r] <- coef(lm(y_list[[r]] ~ -1 + x_list[[r]]))
 }
 
-## ---- plot-b-hat ----
-B_hat <- vb_res$param_list$S %*% vb_res$var_list$pi
-mB_hat <- melt(B_hat)
-colnames(mB_hat) <- c("var", "L1", "beta_hat")
+## ---- merge-fits ----
+m_vb <- melt(t(vb_res$var_list$m))
+m_lm <- melt(t(B_lm))
+colnames(m_vb) <- c("task", "feature", "vb_slope")
+colnames(m_lm) <- c("task", "feature", "lm_slope")
+merged_data_fit  <- merged_data %>%
+  left_join(m_vb) %>%
+  left_join(m_lm)
 
-mB_lm_hat <- melt(B_lm)
-colnames(mB_lm_hat) <- c("var", "L1", "beta_lm_hat")
+m_merged_data_fit <- merged_data_fit %>%
+  melt(measure.vars = c("slope", "vb_slope", "lm_slope"),
+       variable.name = c("slope_type"), value.name = "slope")
 
-mB <- melt(B)
-colnames(mB) <- c("var", "L1", "beta")
-mB <- data.frame(mB, beta_lm_hat = mB_lm_hat$beta_lm_hat,
-                 beta_hat = mB_hat$beta_hat)
+## ---- plot-betas ----
+plot_betas_df <- merged_data_fit %>%
+  filter(time == 1) %>%
+  melt(measure.vars = c("vb_slope", "lm_slope"))
 
-merged_cast <- m_merged %>%
-  dcast(t + L1 + cluster ~ variable) %>%
-  left_join(mB) %>%
-  filter(var == "1", L1 <= 9) %>%
-  arrange(L1, t)
+ggplot(plot_betas_df) +
+  geom_abline(slope = 1, intercept = 0, col = "#696969") +
+  geom_point(aes(x = slope, y = value, col = variable), alpha = 0.5) +
+  facet_wrap(~variable) +
+  coord_fixed() +
+  min_theme
 
-ggplot(merged_cast) +
-  geom_point(aes(x = x.1, y = y, col = cluster), size = .5) +
-  geom_abline(aes(intercept = 0, slope = beta), col = 'purple') +
-  geom_abline(aes(intercept = 0, slope = beta_lm_hat), col = 'steelblue') +
-  geom_abline(aes(intercept = 0, slope = beta_hat), col = 'indianred') +
-  facet_wrap(~L1) +
-  ggtitle("OTU 1 vs. 9 Genes")
+## ---- plot-fitted-reg ----
+ggplot(m_merged_data_fit %>%
+         filter(task < 10, feature < 5)) +
+  geom_point(aes(x = x, y = y, col = as.factor(cluster)), size = .3) +
+  geom_abline(aes(slope = slope, intercept = 0, linetype = slope_type,
+                  col = as.factor(cluster))) +
+  scale_color_manual(values = c("#5FABC8", "#ffdead", "#c16a67")) +
+  facet_grid(feature ~ task) +
+  min_theme
