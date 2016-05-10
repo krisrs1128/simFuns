3a372ce47d787c54780aa2011fc27b710a9e8e6e
Kris Sankaran <sankaran.kris@gmail.com>
2016-05-10 15:27:40 -0700

    revise fukumizu example, what the authors intended I think

--- simMethods/kcca.R
+++ simMethods/kcca.R
@@ -5,25 +5,20 @@
 
 ## ---- kcca-libraries ----
 # List of packages for session
-.packages = c("kernlab",
-              "plyr",
-              "dplyr",
-              "gridExtra",
-              "ggplot2")
+.packages <- c("kernlab", "plyr", "dplyr", "scales", "grid",
+               "gridExtra", "ggplot2", "GGally")
 
 # Install CRAN packages (if not already installed)
 .inst <- .packages %in% installed.packages()
 if(any(!.inst)) {
-  install.packages(.packages[!.inst], repos='http://cran.rstudio.com/')
+  install.packages(.packages[!.inst], repos = "http://cran.rstudio.com/")
 }
 
 # Load packages into session 
-lapply(.packages, require, character.only=TRUE)
+sapply(.packages, require, character.only = TRUE)
 set.seed(04032016)
-
 cat("\014")  # Clear console
-
-rm(list=ls()) # Delete all existing variables
+rm(list = ls()) # Delete all existing variables
 graphics.off() # Close all open plots
 
 ## ---- generate-data ----
@@ -71,6 +66,15 @@ p2 <- ggplot(data.frame(cancor_scores, z)) +
   ggtitle("CCA Correlation between X1, Y1 Scores")
 grid.arrange(p1, p2, ncol = 2)
 
+## ---- cca-evecs ----
+u <- data.frame(t(cancor_res$xcoef))
+ggplot(u) +
+  geom_segment(aes(x = 0, y = 0, xend = X1, yend = X2),
+               arrow = arrow(length = unit(5, "points"))) +
+  ylim(-.15, .15) +
+  xlim(-.15, .15) +
+  coord_fixed(ratio = cancor_res$cor[2] / cancor_res$cor[1])
+
 ## ---- kcca ----
 kcca_res <- kcca(X[[1]], X[[2]],
                  kernel = "rbfdot",
@@ -82,65 +86,68 @@ kcca_scores <- list(X1 = kcca_res@xcoef,
 
 ## ---- kcca-plots ----
 p1 <- ggplot(data.frame(kcca_scores[[1]], z)) +
-  geom_point(aes(x = X1, y = X2, col = z)) +
-  ggtitle("KCCA X-Scores (Nonlinear Example)")
+  geom_point(aes(x = X1, y = X2, col = z))
 p2 <- ggplot(data.frame(kcca_scores)) +
   geom_point(aes(x = X1.1, y = X2.1, col = z)) +
-  ggtitle("KCCA Correlation between X1, Y1 Scores")
+  geom_abline(slope = -1, intercept = 0)
 grid.arrange(p1, p2, ncol = 2)
 
 ## ---- fukumizu-example ----
 # http://www.jmlr.org/papers/volume8/fukumizu07a/fukumizu07a.pdf
-z <- runif(n)
-U <- cbind(z %*% t(c(1 / sqrt(2), 1 / sqrt(2))) + .1 + matrix(rnorm(2 * n, 0, .05), n, 2),
-           z %*% t(c(1 / sqrt(3), sqrt(2 / 3))) + 3 + matrix(rnorm(2 * n, 0, .05), n, 2))
+m <- 1000
+n <- 100
+z <- runif(m)
+u <- z + 0.06 + rnorm(m, 0, .035)
+v <- z + 3 + rnorm(m, 0, .035)
+u <- u[u > 0 & u < 1.5][1:n]
+v <- v[v > 0 & v < 4.1][1:n]
+R <- cbind(sqrt(-4 * log(u / 1.5)), sqrt(-4 * log(v / 4.1)))
 
-R <- scale(U, center = F, scale = c(1.5, 1.5, 4.1, 4.1))
-R <- sqrt(-5 * log(R))
-pairs(R)
+theta <- list("x" = runif(n, 0, 2 * pi),
+              "y" = runif(n, 0, 2 * pi))
+X <- data.frame("x" = c(R[, 1] * cos(theta[["x"]]),
+                        R[, 2] * cos(theta[["y"]])),
+                "y" = c(R[, 1] * sin(theta[["x"]]),
+                        R[, 2] * sin(theta[["y"]])),
+                "cluster" = c(rep("0", n), rep("1", n)))
+
+ggplot(X) +
+  geom_point(aes(x = x, y = y, col = cluster), size = .5, alpha = 0.7) +
+  scale_color_manual(values = c("#008080", "#A9A3D7"))
 
 ## ---- cca-fukumizu ----
-cancor_res <- cancor(R[, 1:2], R[, 3:4])
-cancor_scores <- list(X1 = R[, 1:2] %*% cancor_res$xcoef,
-                      X2 = R[, 3:4] %*% cancor_res$ycoef)
+x_list <- list(X = as.matrix(X[X$cluster == "0", 1:2]),
+               Y = as.matrix(X[X$cluster == "1", 1:2]))
+cancor_res <- cancor(x_list[[1]], x_list[[2]])
+cancor_scores <- list(X1 = x_list[[1]] %*% cancor_res$xcoef,
+                      X2 = x_list[[2]] %*% cancor_res$ycoef)
 
 ## ---- cca-fukumizu-plots ----
 ggplot(data.frame(cancor_scores, z)) +
-  geom_point(aes(x = X1.1, y = X1.2, col = z))
+  geom_point(aes(x = X1.1, y = X1.2, col = z)) +
+  coord_fixed(ratio = cancor_res$cor[2] / cancor_res$cor[1])
 ggplot(data.frame(cancor_scores, z)) +
-  geom_point(aes(x = X2.1, y = X2.2, col = z))
+  geom_point(aes(x = X2.1, y = X2.2, col = z)) +
+  coord_fixed(ratio = cancor_res$cor[2] / cancor_res$cor[1])
 
+ # correlation between scores
 ggplot(data.frame(cancor_scores, z)) +
-  geom_point(aes(x = X1.1, y = X2.1, col = z)) # correlation between scores
+  geom_point(aes(x = X1.1, y = X2.1, col = z)) +
+  coord_fixed()
 
 ## ---- kcca-fukumizu ----
-kcca_res <- kcca(as.matrix(R[, 1:2]), as.matrix(R[, 3:4]),
-                 kpar = list(sigma = 1))
+kcca_res <- kcca(x_list[[1]], x_list[[2]], kpar = list(sigma = .01))
 kcca_scores <- list(X1 = kcca_res@xcoef, X2 = kcca_res@ycoef)
+plot(x_list[[1]][, 1], kcca_scores$X1[, 1])
 
 ## ---- kcca-fukumizu-plots ----
-ggplot(data.frame(kcca_scores, z)) +
-  geom_point(aes(x = X1.1, y = X1.2, col = z))
-ggplot(data.frame(kcca_scores, z)) +
-  geom_point(aes(x = X2.1, y = X2.2, col = z))
+x_res <- data.frame(X, score = c(kcca_scores$X1[, 1], kcca_scores$X2[, 1]))
 
-ggplot(data.frame(kcca_scores, z)) +
-  geom_point(aes(x = X1.1, y = X2.1, col = z))
-ggplot(data.frame(kcca_scores, z)) +
-  geom_point(aes(x = X1.2, y = X2.2, col = z))
+ggplot(x_res) +
+  geom_point(aes(x = x, y = y, col = score, shape = cluster)) +
+  scale_color_gradient(low = "#36191E", high = "#E3809C")
 
-## ---- kcca-fukumizu2 ----
-kcca_res <- kcca(as.matrix(R[, 1:2]), as.matrix(R[, 3:4]), kpar = list(sigma = .4))
-kcca_scores <- list(X1 = kcca_res@xcoef, X2 = kcca_res@ycoef)
-
-## ---- kcca-fukumizu-plots2 ----
-ggplot(data.frame(kcca_scores, z)) +
-  geom_point(aes(x = X1.1, y = X1.2, col = z))
-ggplot(data.frame(kcca_scores, z)) +
-  geom_point(aes(x = X2.1, y = X2.2, col = z))
-
-# remember, don't expect linear correlation
-ggplot(data.frame(kcca_scores, z)) +
-  geom_point(aes(x = X1.1, y = X2.1, col = z))
-ggplot(data.frame(kcca_scores, z)) +
-  geom_point(aes(x = X1.2, y = X2.2, col = z))
+ggplot(data.frame(kcca_scores, R = R[, 1])) +
+  geom_point(aes(x = X1.1, y = X2.1, col = R))
+ggplot(data.frame(kcca_scores, R = R[, 1])) +
+  geom_point(aes(x = X1.2, y = X2.2, col = R))
