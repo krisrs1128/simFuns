4a43eb8d4f979fcf6c53c288cab83e33e44fe60a
Kris Sankaran <sankaran.kris@gmail.com>
2016-10-01 19:47:37 -0700

    Functions for generating HMM data

--- simData/R/hmm.R
+++ simData/R/hmm.R
@@ -2,10 +2,10 @@
 rmultinoulli <- function(p, n = 1) {
   u <- runif(n)
   sum_p <- cumsum(p)
-  stopifnot(sum_p[length(p)] > .999 && sum_p[length(p)] < 1.001)
+  stopifnot(sum_p[length(p)] > .9999 && sum_p[length(p)] < 1.0001)
 
   z <- rep(1, n)
-  for (i in length(p):2) {
+  for (i in length(p):1) {
     z[u <= sum_p[i]] <- i
   }
   z
@@ -28,13 +28,31 @@ markov_chain <- function(P, pi_init = NULL, length = 100) {
   z
 }
 
-#' @ generate many markov chains
+#' @title Generate many markov chains
 #'
 #' @example
 #' # pretty diagonally dominant
 #' P <- matrix(runif(4 * 4), 4, 4) + 10 * diag(4)
 #' P <- diag(1 / rowSums(P)) %*% P
-#' matplot(t(markov_chains(10, P)))
-markov_chains <- function(n, P, pi_init = NULL, length = 100) {
+#' matplot(t(markov_chains(P, n = 3)))
+markov_chains <- function(P, pi_init = NULL, n = 1, length = 100) {
   t(replicate(n,  markov_chain(P, pi_init, length)))
+}
+
+#' @title Generate data from an HMM
+#' @example
+#' obs_densities <- lapply(1:4, function(sigma) { function(n) { rnorm(n, 0, sigma) }})
+#' names(obs_densities) <- 1:4
+#' x <- hmm_data(P, obs_densities, 4)
+#' matplot(t(x))
+hmm_data <- function(P, obs_densities, n = 1, pi_init = NULL, length = 100) {
+  z <- markov_chains(P, pi_init, n, length)
+  x <- matrix(0, nrow(z), ncol(z))
+  K <- nrow(P)
+
+  for (k in seq_len(K)) {
+    n_obs <- sum(z == k)
+    x[z == k] <- obs_densities[[as.character(k)]](n_obs)
+  }
+  x
 }
